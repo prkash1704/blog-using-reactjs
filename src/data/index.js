75aedb14679f2817037d88a54bafe77f5461835f
Prakash Kumar <73516830+prkash1704@users.noreply.github.com>
2022-11-13 14:00:38 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -4,9 +4,9 @@ export const blogData = [
     description:
       "Network effects play a significant role in the cryptocurrency ecosystem. For example, if new users join a particular blockchain network in large numbers, the underlying service likely offers more utility.",
     imageUrl: "/images/pic1.webp",
-    publishDate: "May 17, 2021",
+    publishDate: "October 10, 2022",
     avatar: "/images/avatar.png",
-    authorName: "Anshu Raj",
+    authorName: "the7one",
     tags: ["#network ", "#Effect"],
   },
   {
@@ -14,9 +14,9 @@ export const blogData = [
     description:
       "Polkadot is a blockchain protocol that connects multiple specialized chains into a single universal network.Blockchain is undoubtedly a ground-breaking technology that powers the entire cryptocurrency ecosystem, including Decentralized Finance.",
     imageUrl: "/images/pic2.webp",
-    publishDate: "May 9, 2021",
+    publishDate: "October 11, 2022",
     avatar: "/images/avatar.png",
-    authorName: "Anshu Raj",
+    authorName: "the7one",
     tags: ["#Polkadot ", "#DOT"],
   },
   {
@@ -24,9 +24,9 @@ export const blogData = [
     description:
       "Before discussing what NFTs are and how they work, let's first understand the difference between the terms 'fungible' and 'non-fungible.'Fungible assets are mutually interchangeable and can be replaced by another identical asset. For example, Bitcoin, Ethereum, or even traditional money are fungible",
     imageUrl: "/images/pic3.webp",
-    publishDate: "May 7, 2021",
+    publishDate: "October 12, 2022",
     avatar: "/images/avatar.png",
-    authorName: "Anshu Raj",
+    authorName: "the7one",
     tags: ["#NFTs ", "#assets"],
   },
 ];
